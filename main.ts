c9570aafceb96e1c94da486e5dc99e3ad8f793f3
sera <serotoninoy@gmail.com>
2024-11-27 07:00:19 +0700

    Initial commit

---
+++ main.ts
@@ -0,0 +1,16 @@
+import { App, fsRoutes, staticFiles, trailingSlashes } from "fresh";
+import { type State } from "/utils/core.ts";
+
+export const app = new App<State>();
+app.use(staticFiles());
+app.use(trailingSlashes("never"));
+
+await fsRoutes(app, {
+	dir: "./",
+	loadIsland: (path) => import(`./islands/${path}`),
+	loadRoute: (path) => import(`./routes/${path}`),
+});
+
+if (import.meta.main) {
+	await app.listen();
+}
---
+++ routes/_app.tsx
@@ -0,0 +1,20 @@
+import { define } from "/utils/core.ts";
+
+export default define.page(({ Component }) => {
+	return (
+		<html>
+			<head>
+				<meta charset="utf-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+				<title>www</title>
+				<link rel="stylesheet" href="/styles.css" />
+			</head>
+			<body>
+				<Component />
+			</body>
+		</html>
+	);
+});
