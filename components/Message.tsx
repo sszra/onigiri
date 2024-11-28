4cf207773edeaab8b323c81ad15cffbda6f9f5d4
sera <serotoninoy@gmail.com>
2024-11-28 07:24:29 +0000

    fix message break lines

--- components/Message.tsx
+++ components/Message.tsx
@@ -2,8 +2,15 @@ import { APIMessage } from "~/utils/chat.ts";
 
 export function Message({ data }: { data: APIMessage }) {
 	return (
-		<div class="bg-slate-100 p-3">
-			<p>{data.content}</p>
+		<div class="flex flex-col bg-slate-100 p-3">
+			<p>
+				{data.content.split("\n").map((content) => (
+					<>
+						{content}
+						<br />
+					</>
+				))}
+			</p>
 		</div>
 	);
 }
