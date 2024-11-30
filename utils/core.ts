177e429e738fa312761acaaaa9783e320cacea77
sera <serotoninoy@gmail.com>
2024-11-30 01:56:22 +0000

    utility for required environment variable

--- utils/core.ts
+++ utils/core.ts
@@ -5,3 +5,19 @@ export interface State {}
 
 export const define = createDefine<State>();
 export const kv = await Deno.openKv();
+
+export function env(name: string, required: true): string;
+export function env(name: string, required?: false): string | undefined;
+export function env(name: string, required?: boolean): string | undefined {
+	const env = Deno.env.get(name);
+
+	if (required) {
+		if (!env) {
+			throw new Error(`Environment variable not found: "${name}"`);
+		} else {
+			return env;
+		}
+	} else {
+		return env;
+	}
+}
